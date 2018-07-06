58533db18a17287dabf866cb710af7e8d4daac74
Christopher <chrisformey@yahoo.com>
2018-07-06 03:02:39 -0500

    unable to get text to write to screen

--- Crosshair.py
+++ Crosshair.py
@@ -10,6 +10,8 @@ clock = pygame.time.Clock()
 
 white = (255,255,255)
 
+black = (0,0,0)
+
 screen_width = 800
 
 screen_height = 600
@@ -26,8 +28,17 @@ screen.blit(crosshair, middle_of_screen)
 
 gun_sound = pygame.mixer.Sound('gunshot.ogg')
 
+font = pygame.font.Font("freesansbold.ttf", 100)
+
+shots = 0
+
+
+
 while True:
 
+	font_surface = font.render("Shots fired: " + str(shots), False, black)
+
+	screen.blit(font_surface, (400, 300))
 	pygame.mouse.set_visible(False)
 
 	screen.fill(white)
@@ -43,6 +54,7 @@ while True:
 
 		elif event.type == MOUSEBUTTONDOWN:
 			gun_sound.play()
+			shots += 1
 
 
 	pygame.display.update()
